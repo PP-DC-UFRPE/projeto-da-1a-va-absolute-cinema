71c6e0be9b21175c4f493df8820df8ee4ff38fd9
danilodepadua <pwa.danilo@gmail.com>
2024-12-24 14:33:44 -0300

    Modulo de tipos

--- Exibir_filmes.hs
+++ Exibir_filmes.hs
@@ -1,6 +1,20 @@
 module Exibir_filmes
 (exibicao) where
+import Tipos
+
+jaws = Filme "Jaws" ["Acao"] 90 "hahah" 
+s = Sessao jaws (12, 40) Dublado True 2 [('d', 2, False)]
+
+--printar :: Cliente -> String
+--printar (Cliente n c i o) = "Nome: " ++ n ++ ", CPF: " ++ c
+
+printar :: Sessao -> String
+printar (Sessao f h t i s a) = "Filme: " ++ show f
+
+pegarTitulo :: Sessao -> String
+pegarTitulo (Sessao (Filme t _ _ _) _ _ _ _ _ ) = t --acho inteligente criar um arquivo de utilidades com funções desse tipo
 
 exibicao :: IO()
 exibicao = do
-    putStrLn "Jaws"
+    putStrLn(pegarTitulo s)
+    putStrLn(printar s)
--- Main.hs
+++ Main.hs
@@ -1,34 +1,6 @@
 module Main where
 
-import Data.List
 import Loop (loop)
-
-type Nome = String
-type Cpf = String
-type Idade = Int
-data Ocupacao = Estudante | Professor | Outras deriving Show
-data Cliente = Cliente Nome Cpf Idade Ocupacao deriving Show
-
-type Titulo = String
-type Genero = [String]
-type Duracao = Int -- Minutos
-type Sinopse = String
-data Filme = Filme Titulo Genero Duracao Sinopse deriving Show
-
-type Horario = (Int, Int) -- Hora/Minuto
-type Dia = (Int, Int, Int) -- Dia/Mes/Ano
-data TipoSessao = Dublado | Legendado deriving Show
-type Is3D = Bool 
-type Sala = Int
-type Assento = (Char, Int, Bool) -- Letra da Fileira/Numero Assento/Ocupado
-data Sessao = Sessao Filme Horario TipoSessao Is3D Sala [Assento] deriving Show
-
-data TipoIngresso = Inteira Float | Meia deriving Show
-type Ingresso = (TipoIngresso, Assento)
-type Valor = Float
-data Pedido = Ped Cliente Sessao [Ingresso] Valor deriving Show
-
-type Sistema = ([Cliente],[Filme],[Sessao],[Pedido])
 
 main :: IO ()
 main = loop
