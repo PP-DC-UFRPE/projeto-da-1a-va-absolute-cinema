644cf0b6c0e409848072f79ebec55c81313be910
Alan Torres <alanvictor.torres@gmail.com>
2025-01-30 15:44:46 -0300

    fix: Correção nas função parse e format

--- Utils.hs
+++ Utils.hs
@@ -5,6 +5,13 @@ import Data.List (find)
 import Data.List (intercalate)
 import Text.Read (readMaybe)
 
+-- Função auxiliar para dividir strings em partes com base em um delimitador
+split :: Char -> String -> [String]
+split delim str =
+    case break (== delim) str of
+        (part, "") -> [part]
+        (part, _:rest) -> part : split delim rest
+
 --- Funções de Mapeamento dos objetos
 -- Função para carregar clientes de um arquivo
 parseCliente :: String -> Cliente
@@ -60,13 +67,6 @@ parsePedido clientes sessoes linha =
             return $ Ped idPedido cliente sessao ingressos valor
         _ -> Nothing
 
--- Função auxiliar para dividir strings em partes com base em um delimitador
-split :: Char -> String -> [String]
-split delim str =
-    case break (== delim) str of
-        (part, "") -> [part]
-        (part, _:rest) -> part : split delim rest
-
 -- Função para mapear o campo de gêneros de um filme de uma string para uma lista de strings
 -- Exemplo: "[Aventura,Ficção]" -> ["Aventura", "Ficção"]
 parseGenero :: String -> [String]
@@ -98,45 +98,6 @@ agrupaAssentos :: [String] -> [[String]]
 agrupaAssentos [] = []
 agrupaAssentos (a:b:c:xs) = [a, b, c] : agrupaAssentos xs
 agrupaAssentos _ = error "Formato inválido de assento."
-
-formatarCliente :: Cliente -> String
-formatarCliente (Cliente nome cpf idade ocupacao) =
-    nome ++ ";" ++ cpf ++ ";" ++ show idade ++ ";" ++ show ocupacao
-
-formatarFilme :: Filme -> String
-formatarFilme (Filme id titulo genero duracao sinopse) =
-    show id ++ ";" ++ titulo ++ ";" ++ formatarGenero genero ++ ";" ++ show duracao ++ ";" ++ sinopse
-
-formatarGenero :: Genero -> String
-formatarGenero genero = "[" ++ unwords (map (\g -> g ++ ",") (init genero)) ++ last genero ++ "]"
-
-formatarSessao :: Sessao -> String
-formatarSessao (Sessao id (Filme idFilme _ _ _ _) horario dia tipoSessao is3D sala assentos) =
-    show id ++ ";" ++ show idFilme ++ ";" ++ formatarHorario horario ++ ";" ++ formatarDia dia ++ ";"
-        ++ formatarTipo tipoSessao ++ ";" ++ show is3D ++ ";" ++ show sala ++ ";" ++ formatarAssentos assentos
-
--- Formata um único assento
-formatarAssento :: Assento -> String
-formatarAssento (fileira, numero, ocupado) =
-    "[" ++ [fileira] ++ "," ++ show numero ++ "," ++ show ocupado ++ "]"
-    -- Formata os assentos no formato esperado
-formatarAssentos :: [Assento] -> String
-formatarAssentos assentos = "[" ++ intercalate "," (map formatarAssento assentos) ++ "]"
-
--- Formata o horário no formato HH:MM
-formatarHorario :: Horario -> String
-formatarHorario (h, m) = show h ++ ":" ++ (if m < 10 then "0" ++ show m else show m)
-
--- Formata a data no formato DD/MM/AAAA
-formatarDia :: Dia -> String
-formatarDia (d, m, a) =
-    let formatar n = if n < 10 then "0" ++ show n else show n
-    in formatar d ++ "/" ++ formatar m ++ "/" ++ show a
-
--- Formata o tipo de sessão (Dublado ou Legendado)
-formatarTipo :: TipoSessao -> String
-formatarTipo Dublado = "Dublado"
-formatarTipo Legendado = "Legendado"
 
 -- Função para mapear a string de ingressos em uma lista de Ingresso
 parseIngressos :: String -> Maybe [Ingresso]
@@ -175,6 +136,50 @@ parseAssento s =
             o <- readMaybe oStr
             Just (f, n, o)
         _ -> Nothing
+
+-- Formatar Cliente
+formatarCliente :: Cliente -> String
+formatarCliente (Cliente nome cpf idade ocupacao) =
+    nome ++ ";" ++ cpf ++ ";" ++ show idade ++ ";" ++ show ocupacao
+
+-- Formata Filme
+formatarFilme :: Filme -> String
+formatarFilme (Filme id titulo genero duracao sinopse) =
+    show id ++ ";" ++ titulo ++ ";" ++ formatarGenero genero ++ ";" ++ show duracao ++ ";" ++ sinopse
+
+-- Formata o genero no formato [Ação, Drama]
+formatarGenero :: Genero -> String
+formatarGenero genero = "[" ++ unwords (map (\g -> g ++ ",") (init genero)) ++ last genero ++ "]"
+
+-- Formata Sessão
+formatarSessao :: Sessao -> String
+formatarSessao (Sessao id (Filme idFilme _ _ _ _) horario dia tipoSessao is3D sala assentos) =
+    show id ++ ";" ++ show idFilme ++ ";" ++ formatarHorario horario ++ ";" ++ formatarDia dia ++ ";"
+        ++ formatarTipo tipoSessao ++ ";" ++ show is3D ++ ";" ++ show sala ++ ";" ++ formatarAssentos assentos
+
+-- Formata um único assento
+formatarAssento :: Assento -> String
+formatarAssento (fileira, numero, ocupado) =
+    "[" ++ [fileira] ++ "," ++ show numero ++ "," ++ show ocupado ++ "]"
+
+-- Formata os assentos no formato esperado
+formatarAssentos :: [Assento] -> String
+formatarAssentos assentos = "[" ++ intercalate "," (map formatarAssento assentos) ++ "]"
+
+-- Formata o horário no formato HH:MM
+formatarHorario :: Horario -> String
+formatarHorario (h, m) = show h ++ ":" ++ (if m < 10 then "0" ++ show m else show m)
+
+-- Formata a data no formato DD/MM/AAAA
+formatarDia :: Dia -> String
+formatarDia (d, m, a) =
+    let formatar n = if n < 10 then "0" ++ show n else show n
+    in formatar d ++ "/" ++ formatar m ++ "/" ++ show a
+
+-- Formata o tipo de sessão (Dublado ou Legendado)
+formatarTipo :: TipoSessao -> String
+formatarTipo Dublado = "Dublado"
+formatarTipo Legendado = "Legendado"
 
 -- Função para formatar um Pedido em uma string
 formatarPedido :: Pedido -> String
